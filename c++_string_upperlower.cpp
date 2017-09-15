dc328279502a2648521c79a7c931abc847ec56e1
SteveDengZishi <zd475@nyu.edu>
2017-09-15 18:54:24 -0400

    stringConversion

---
+++ c++_string_upperlower.cpp
@@ -0,0 +1,86 @@
+//
+//  2.cpp
+//  HackerRankQ
+//
+//  Created by Steve DengZishi on 5/30/17.
+//  Copyright © 2017 Steve DengZishi. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath> // To use sqrt()
+#include <list> // To use STL linked-list in dfs topological sort
+#include <stack> // To use STL stack in topological sort
+#include <queue> // To use STL queue in bfs, dijkstra's, maxflow
+#include <cstring> // To use memset()
+#include <cstdlib> // To use atoi(), abs(), malloc()
+#include <algorithm> // To use sort(), next_permutation(), min(), max() etc.
+//#include <unordered_map> // To allow O(1) mapping access using key->value
+#include <set> // To sort and remove duplicate when inserted
+#include <unordered_set> //To remove duplicates and count size
+#include <fstream> //To use c++ style read stream from file
+
+//macro here
+#define FOR(i,a,b) for(size_t i=a;i<b;i++)
+#define DE(x) cout << #x << ":" << x << endl
+#define DEIF cout<<"DEBUG: in if"<<endl
+#define DEELSE cout<<"DEBUG: in else"<<endl
+#define DEIFIF cout<<"DEBUG: in if if"<<endl
+#define DEIFELSE cout<<"DEBUG: in if else"<<endl
+#define DEELSEIF cout<<"DEBUG: in else if"<<endl
+#define DEELSEELSE cout<<"DEBUG: in else else"<<endl
+#define ALL(a) a.begin(),a.end()
+#define CINLINE(a) getline(cin,a)
+#define FILL(a,b) memset(a, b , sizeof(a)) //fill array a with all bs
+#define INIT(a) FILL(a,0) //initialize array a with all 0s
+
+//name space here
+using namespace std;
+
+//typedef here
+typedef vector<int> VI;
+typedef vector<pair<int,pair<int,int>>> Edges;
+typedef pair<int,int> PII;
+
+//debugging functions here
+template<class T>
+void DEVEC(vector<T> vec){
+    FOR(i,0,vec.size()){
+        DE(vec[i]);
+    }
+    cout<<endl;
+}
+
+template<class T>
+void DEARRAY(T array[],int num=20){
+    FOR(i,0,num+1){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+//functions, global variables, comparators & Non-STL Data Structures definition here
+
+
+//start of main()
+int main(int argc, const char * argv[]) {
+    //optimize iostream
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //reminder to clear vector between test cases
+    //Your code here
+    string a;
+    cin>>a;
+    int countUpper=0;
+    FOR(i,0,a.length()){
+        if(isupper(a[i])) countUpper++;
+    }
+    int countLower=a.length()-countUpper;
+    if(countUpper>countLower) transform(a.begin(), a.end(), a.begin(), ::toupper);
+    else transform(a.begin(), a.end(), a.begin(), ::tolower);;
+    cout<<a<<endl;
+    
+    return 0;
+}
